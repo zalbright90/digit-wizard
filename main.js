9612a0ed8cb9f5f0824103aec59b5b9c7d7e6335
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-07-14 04:56:15 -0700

    Set up operate function using variables

--- main.js
+++ main.js
@@ -2,21 +2,34 @@ let firstNumber = " ";
 let secondNumber = " ";
 let operator = " ";
 
-function add(a, b) {
-    return a + b;
-};
-
-function subtract(a, b) {
-    return a - b;
-};
-
-function multiply(a, b) {
-    return a * b;
-};
-
-function divide(a, b) {
-    if (b === 0) {
-        return "By the ancient laws, no division by void";
+function operate(firstNumber, operator, secondNumber) {
+    switch(operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
     }
-    return a / b;
+    
+    function add(a, b) {
+        return a + b;
+    };
+    
+    function subtract(a, b) {
+        return a - b;
+    };
+    
+    function multiply(a, b) {
+        return a * b;
+    };
+    
+    function divide(a, b) {
+        if (b === 0) {
+            return "By the ancient laws, no division by void";
+        }
+        return a / b;
+    }
 }
