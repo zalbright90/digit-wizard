b8065524ca798d18ff439421f2fb6a0972fd7843
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-07-14 04:35:14 -0700

    Add the functions for subtract, multiply, and divide

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 function add(a, b) {
     return a + b;
 };
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    if (b === 0) {
+        return "By the ancient laws, no division by void";
+    }
+    return a / b;
+}
