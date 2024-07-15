7f81e578132c635283ee11b01727a52f301d76df
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-07-15 04:02:32 -0700

    Add functions to update display

--- main.js
+++ main.js
@@ -1,6 +1,56 @@
+let displayValue = "0";
+
+const displayOutput = document.querySelector('#output');
+
+window.addEventListener('keydown', function(e){
+    const key = document.querySelector(`button[data-key='${e.keyCode}']`);
+    key.click();
+});
+
+function updateDisplay(value) {
+    if (displayValue === "0") {
+        displayValue = "";
+    }
+    displayValue += value;
+    displayOutput.textContent = displayValue;
+}
+
+document.querySelector('.zero').addEventListener('click', () => updateDisplay('0'));
+
+document.querySelector('.one').addEventListener('click', () => updateDisplay('1'));
+
+document.querySelector('.two').addEventListener('click', () => updateDisplay('2'));
+
+document.querySelector('.three').addEventListener('click', () => updateDisplay('3'));
+
+document.querySelector('.four').addEventListener('click', () => updateDisplay('4'));
+
+document.querySelector('.five').addEventListener('click', () => updateDisplay('5'));
+
+document.querySelector('.six').addEventListener('click', () => updateDisplay('6'));
+
+
+document.querySelector('.seven').addEventListener('click', () => updateDisplay('7'));
+
+document.querySelector('.eight').addEventListener('click', () => updateDisplay('8'));
+
+document.querySelector('.nine').addEventListener('click', () => updateDisplay('9'));
+
+document.querySelector('dot').addEventListener('click', () => {
+    if (!displayValue.includes('.')) {
+        updateDisplay('.');
+    }
+});
+
+document.querySelector('.clear').addEventListener('click', () => {
+    if (displayValue = "0");
+    displayOutput.textContent = "displayValue";
+});
+
 let firstNumber = " ";
 let secondNumber = " ";
 let operator = " ";
+
 
 function operate(firstNumber, operator, secondNumber) {
     switch(operator) {
